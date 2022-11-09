6f1eed0e8d1b2fee023d65e1bd5347eacdddc8d2
Andrew Mary Huet de Barochez <phoqueeberlue@gmail.com>
2022-11-09 09:08:28 +0100

    Adding code to saves the public key and private key into files

--- src/RSA.py
+++ src/RSA.py
@@ -9,14 +9,18 @@ def generatePrivateKey():
     return publicKey, privateKey
 
 
-a, b = generatePrivateKey()
-print(type(a))
-print(a)
-print(type(b))
-print(b)
+publicKey, privateKey = generatePrivateKey()
 
-print(a.public_bytes
-      (
-          encoding=serialization.Encoding.PEM,
-          format=serialization.PublicFormat.SubjectPublicKeyInfo)
-      )
+with open("../id_rsa", "wb") as file:
+    file.write(privateKey.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    ))
+
+# Write the public key in the id_ras.pub file
+with open("../id_rsa.pub", "wb") as file:
+    file.write(publicKey.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo)
+    )
