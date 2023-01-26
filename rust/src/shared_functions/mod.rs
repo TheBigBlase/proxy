6938a734651212e3cc255fae6f377e27b709b8cf
Andrew Mary Huet de Barochez <phoqueeberlue@gmail.com>
2023-01-26 21:38:44 +0100

    Refactored the crypto code using structs and impls

--- rust/src/shared_functions/mod.rs
+++ rust/src/shared_functions/mod.rs
@@ -1,69 +1,174 @@
-use openssl::pkey::{Public, Private};
-use openssl::error::ErrorStack;
-use openssl::rsa::{Rsa, Padding};
+use openssl::pkey::{Private, Public};
+use openssl::rand::rand_bytes;
+use openssl::rsa::{Padding, Rsa};
+use openssl::symm::{decrypt, encrypt, Cipher};
 use std::fs::File;
-use std::io::{Write, Read};
+use std::io::{Read, Write};
 use std::path::Path;
 
-// Generate rsa public and private keys and store them
-pub fn generate_rsa_keys(pub_path: &str, priv_path: &str) -> Result<Rsa<Private>, ErrorStack> {
-
-    let pub_path = Path::new(pub_path);
-    let priv_path = Path::new(priv_path);
-
-    let rsa = Rsa::generate(2048)?;
-    let mut pub_file = File::create(pub_path).unwrap();
-    let mut priv_file = File::create(priv_path).unwrap();
-
-    let res = pub_file.write(&rsa.public_key_to_pem().unwrap());
-    let res = priv_file.write(&rsa.private_key_to_pem().unwrap());
-
-    Ok(rsa)
+pub struct Asym {
+    private_key: Option<Rsa<Private>>,
+    public_key: Option<Rsa<Public>>,
 }
 
-// Load the public rsa key from a file with pem format
-pub fn load_rsa_public_key_from_file(public_key_path: &str) -> Result<Rsa<Public>, ErrorStack> {
-    
-    let public_key_path = Path::new(public_key_path);
+impl Asym {
+    // Generate rsa public and private keys and store them
+    pub fn new() -> Asym {
+        let rsa = Rsa::generate(2048).unwrap();
 
-    let mut public_key_file: File = File::open(public_key_path).unwrap();
-    let buffer = &mut Vec::new();
-    public_key_file.read_to_end(buffer).unwrap();
+        Asym {
+            private_key: Some(rsa),
+            public_key: None,
+        }
+    }
 
-    let public_key = Rsa::public_key_from_pem(buffer)?;
+    // Load the private rsa key from a file with pem format
+    pub fn load_from_priv_file(private_key_path: &str) -> Asym {
+        let private_key_path = Path::new(private_key_path);
 
-    Ok(public_key)
+        let mut private_key_file: File = File::open(private_key_path).unwrap();
+        let buffer = &mut Vec::new();
+        private_key_file.read_to_end(buffer).unwrap();
+
+        let private_key = Rsa::private_key_from_pem(buffer).unwrap();
+
+        Asym {
+            private_key: Some(private_key),
+            public_key: None,
+        }
+    }
+
+    // Load the public rsa key from a file with pem format
+    pub fn load_from_pub_file(public_key_path: &str) -> Asym {
+        let public_key_path = Path::new(public_key_path);
+
+        let mut public_key_file: File = File::open(public_key_path).unwrap();
+        let buffer = &mut Vec::new();
+        public_key_file.read_to_end(buffer).unwrap();
+
+        let public_key = Rsa::public_key_from_pem(buffer).unwrap();
+
+        Asym {
+            private_key: None,
+            public_key: Some(public_key),
+        }
+    }
+
+    pub fn save_keys(&self, pub_path: &str, priv_path: &str) {
+        let pub_path = Path::new(pub_path);
+        let priv_path = Path::new(priv_path);
+
+        let mut pub_file = File::create(pub_path).unwrap();
+        let mut priv_file = File::create(priv_path).unwrap();
+
+        let res = pub_file.write(
+            &self
+                .private_key
+                .as_ref()
+                .unwrap()
+                .public_key_to_pem()
+                .unwrap(),
+        );
+        let res = priv_file.write(
+            &self
+                .private_key
+                .as_ref()
+                .unwrap()
+                .private_key_to_pem()
+                .unwrap(),
+        );
+    }
+
+    /// Encrypts data using the public rsa key, returning the encrypted text and the number of
+    /// encrypted bytes
+    pub fn encrypt(&self, data: &[u8]) -> Vec<u8> {
+        let mut buf = vec![0; self.public_key.as_ref().unwrap().size() as usize];
+        let encrypted_len = self
+            .public_key
+            .as_ref()
+            .unwrap()
+            .public_encrypt(data, &mut buf, Padding::PKCS1)
+            .unwrap();
+
+        buf
+    }
+
+    /// Decrypts data using the private rsa key, returning the decrypted text and the number of
+    /// decrypted bytes
+    pub fn decrypt(&self, encrypted_text: &[u8]) -> Vec<u8> {
+        let mut decrypted_text = vec![0; self.private_key.as_ref().unwrap().size() as usize];
+        let decrypted_len = self
+            .private_key
+            .as_ref()
+            .unwrap()
+            .private_decrypt(encrypted_text, &mut decrypted_text, Padding::PKCS1)
+            .unwrap();
+
+        decrypted_text
+    }
+
+    pub fn get_pem_pub(&self) -> Vec<u8> {
+        match self.private_key.as_ref() {
+            Some(_) => {
+                return self
+                    .private_key
+                    .as_ref()
+                    .unwrap()
+                    .public_key_to_pem()
+                    .unwrap()
+            }
+            None => {
+                return self
+                    .public_key
+                    .as_ref()
+                    .unwrap()
+                    .public_key_to_pem()
+                    .unwrap()
+            }
+        }
+    }
+
+    pub fn get_pem_priv(&self) -> Vec<u8> {
+        self.private_key
+            .as_ref()
+            .unwrap()
+            .private_key_to_pem()
+            .unwrap()
+    }
 }
 
-// Load the private rsa key from a file with pem format
-pub fn load_rsa_private_key_from_file(private_key_path: &str) -> Result<Rsa<Private>, ErrorStack> {
-
-    let private_key_path = Path::new(private_key_path);
-
-    let mut private_key_file: File = File::open(private_key_path).unwrap();
-    let buffer = &mut Vec::new();
-    private_key_file.read_to_end(buffer).unwrap();
-
-    let private_key = Rsa::private_key_from_pem(buffer)?;
-
-    Ok(private_key)
+pub struct Sym {
+    cipher: Cipher,
+    key: [u8; 16],
 }
 
-/// Encrypts data using the public rsa key, returning the encrypted text and the number of
-/// encrypted bytes
-pub fn encrypt_rsa(key: Rsa<Public>, data: &[u8]) -> Result<(Vec<u8>, usize), ErrorStack> { 
-    let mut buf = vec![0; key.size() as usize];
-    let encrypted_len = key.public_encrypt(data, &mut buf, Padding::PKCS1)?;
+impl Sym {
+    pub fn new() -> Sym {
+        let mut buf = [0; 16];
+        rand_bytes(&mut buf).unwrap();
 
-    Ok((buf, encrypted_len))
+        Sym {
+            cipher: Cipher::aes_128_cbc(),
+            key: buf,
+        }
+    }
+
+    pub fn from_key(key: [u8; 16]) -> Sym {
+        Sym {
+            cipher: Cipher::aes_128_cbc(),
+            key: key,
+        }
+    }
+
+    pub fn get_key(&self) -> [u8; 16] {
+        self.key
+    }
+
+    pub fn encrypt(&self, data: &[u8]) -> Vec<u8> {
+        encrypt(self.cipher, &self.key, None, data).unwrap()
+    }
+
+    pub fn decrypt(&self, data: &[u8]) -> Vec<u8> {
+        decrypt(self.cipher, &self.key, None, data).unwrap()
+    }
 }
-
-/// Decrypts data using the private rsa key, returning the decrypted text and the number of
-/// decrypted bytes
-pub fn decrypt_rsa(key: Rsa<Private>, encrypted_text: &[u8]) -> Result<(Vec<u8>, usize), ErrorStack> { 
-    let mut decrypted_text = vec![0; key.size() as usize];
-    let decrypted_len = key.private_decrypt(encrypted_text, &mut decrypted_text, Padding::PKCS1)?;
-
-    Ok((decrypted_text, decrypted_len))
-}
-
--- rust/src/test_crypto.rs
+++ rust/src/test_crypto.rs
@@ -1,20 +1,36 @@
 mod shared_functions;
+
+use shared_functions::{ Asym, Sym };
 use std::path::Path;
 
 fn main() -> std::io::Result<()> {
     let pub_path = "id_rsa.pub";
     let priv_path = "id_rsa";
 
-    let _private_key = shared_functions::generate_rsa_keys(pub_path, priv_path).unwrap();
+    let asym = Asym::new();
 
-    let public_key = shared_functions::load_rsa_public_key_from_file(pub_path).unwrap();
-    let private_key = shared_functions::load_rsa_private_key_from_file(priv_path).unwrap();
+    let _private_key = asym.save_keys(pub_path, priv_path);
 
-    let (encrypted_text, len_encrypted) = shared_functions::encrypt_rsa(public_key, b"IN RUST WE TRUST").unwrap();
+    let asym_pub = Asym::load_from_pub_file(pub_path);
+    let asym_priv = Asym::load_from_priv_file(priv_path);
+
+    let encrypted_text = asym_pub.encrypt(b"IN RUST WE TRUST");
     println!("{:?}", String::from_utf8_lossy(&encrypted_text));
 
-    let (decrypted_text, len_decrypted) = shared_functions::decrypt_rsa(private_key, &encrypted_text).unwrap();
+    let decrypted_text = asym_priv.decrypt(&encrypted_text);
     println!("{:?}", String::from_utf8_lossy(&decrypted_text));
+
+    let data = b"Test symetric crypting";
+
+    let sym = Sym::new();
+
+    let encrypted_text_sym = sym.encrypt(data);
+
+    println!("{:?}", String::from_utf8_lossy(&encrypted_text_sym));
+
+    let decrypted_text_sym = sym.decrypt(encrypted_text_sym.as_slice());
+
+    println!("{:?}", String::from_utf8_lossy(&decrypted_text_sym));
 
     Ok(())
 }
